514f162eed85e130c767754867f4c7c509b5d543
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2022-06-26 21:30:54 -0500

    Change state in demo app

--- gst-app/main.c
+++ gst-app/main.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 
 #include <gst/gst.h>
+#include <unistd.h>
 
-#define PIPELINE_LAUNCH "fakesrc ! fake_filter ! fakesink"
+#define PIPELINE_LAUNCH "videotestsrc ! fake_filter ! fakesink"
 
 static GstElement* pipeline = NULL;
 
@@ -16,7 +17,14 @@ int main(int argc, char** argv) {
         printf("gst_parse_launch error: %s\n", error->message);
         return EXIT_FAILURE;
     }
-    g_assert(GST_IS_PIPELINE(pipeline));
+    g_assert(pipeline && GST_IS_PIPELINE(pipeline));
+
+    GstStateChangeReturn ret;
+    ret = gst_element_set_state(GST_ELEMENT(pipeline), GST_STATE_PLAYING);
+    g_assert(ret != GST_STATE_CHANGE_FAILURE);
+    sleep(1);
+    ret = gst_element_set_state(GST_ELEMENT(pipeline), GST_STATE_NULL);
+    g_assert(ret != GST_STATE_CHANGE_FAILURE);
 
     // test shutdown
     gst_object_unref(G_OBJECT(pipeline));
